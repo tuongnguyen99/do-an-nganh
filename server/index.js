d76638d0db31a3b3567a3f1c1ea1c07d6cb95a5d
tuongnguyen <nctuong99@gmail.com>
2020-05-15 20:00:19 +0700

    update

--- server/index.js
+++ server/index.js
@@ -29,6 +29,27 @@ convertToObject = (string) => {
   };
 };
 
+const dayMap = {
+  'Thứ Hai': 0,
+  'Thứ Ba': 1,
+  'Thứ Tư': 2,
+  'Thứ Năm': 3,
+  'Thứ Sáu': 4,
+  'Thứ Bảy': 5,
+  'Chủ Nhật': 6,
+};
+
+const timeMap = [7, 7.45, 8.3, 9.45, 10, 1.45];
+
+getDate = (startDate, weekDay) => {
+  startDate = startDate.split('/');
+  var date = new Date(`${startDate[2]}/${startDate[1]}, ${startDate[0]}`);
+  date.setDate(date.getDate() + dayMap[weekDay]);
+  console.log(' asf ' + date.getDate());
+
+  return date;
+};
+
 // get data from html
 getData = (html) => {
   const $ = cheerio.load(html);
@@ -39,19 +60,40 @@ getData = (html) => {
     .text()
     .split(' - ');
   const detail = $('#ctl00_ContentPlaceHolder1_ctl00_lblContentLopSV').text();
+  const date = $('select[name$="ddlTuan"] option:nth-child(2)').text();
+  const [startDate, endDate] = date.match(
+    /(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}/g
+  );
 
   const schedule = $(
     '#ctl00_ContentPlaceHolder1_ctl00_Table1 tbody td'
   ).toArray();
 
-  const data = schedule.reduce((acc, element) => {
+  let data = schedule.reduce((acc, element) => {
     const el = $(element);
     let content = el.attr('onmouseover');
     content ? acc.push(convertToObject(content)) : acc;
     return acc;
   }, []);
 
-  return { id, name, dateOfBirth, detail, schedule: data };
+  data = data.map((scheduleItem) => {
+    const date = getDate(startDate, scheduleItem.weekDay);
+    return { ...scheduleItem, date };
+  });
+
+  return {
+    week: {
+      from: startDate,
+      to: endDate,
+    },
+    student: {
+      id,
+      name,
+      dateOfBirth,
+      detail,
+    },
+    schedule: data,
+  };
 };
 
 app.get('/api/schedule/:id', (req, res) => {
